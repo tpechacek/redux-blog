38c376a6091c379773a735478105f99b65b9e5b8
Taylor Pechacek <taylortpechacek@gmail.com>
2017-05-18 23:44:08 +1000

    Getting react router wired up to start

--- src/index.js
+++ src/index.js
@@ -2,14 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+import promise from 'redux-promise';
 
-import App from './components/app';
 import reducers from './reducers';
+import PostsIndex from './components/posts_index';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+class Hello extends React.Component {
+	render() {
+		return <div>Hello!</div>
+	}
+}
+
+class Goodbye extends React.Component {
+	render() {
+		return <div>Goodbye!</div>
+	}
+}
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+    	<div>
+	    	<Route path="/" component={PostsIndex} />
+    	</div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
