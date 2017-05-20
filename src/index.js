ac350b9b2b6ac56456eb8294dbcf249eb5eadb57
Taylor Pechacek <taylortpechacek@gmail.com>
2017-05-20 17:38:13 +1000

    adding the new post component & basic routing

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import promise from 'redux-promise';
 
 import reducers from './reducers';
 import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -26,7 +27,10 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
     	<div>
-	    	<Route path="/" component={PostsIndex} />
+    		<Switch>
+	    		<Route exact path="/" component={PostsIndex} />
+					<Route path="/posts/new" component={PostsNew} />
+				</Switch>
     	</div>
     </BrowserRouter>
   </Provider>
