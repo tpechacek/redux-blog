4fbed5fa59c6f5cc9aab6fbbbcb1e6d82eb67894
Taylor Pechacek <taylortpechacek@gmail.com>
2017-05-21 21:51:09 +1000

    making progress on the form and hooking up routing

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import promise from 'redux-promise';
 import reducers from './reducers';
 import PostsIndex from './components/posts_index';
 import PostsNew from './components/posts_new';
+import PostsShow from './components/posts_show';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -30,6 +31,7 @@ ReactDOM.render(
     		<Switch>
 	    		<Route exact path="/" component={PostsIndex} />
 					<Route path="/posts/new" component={PostsNew} />
+					<Route path="/posts/:id" component={PostsShow} />
 				</Switch>
     	</div>
     </BrowserRouter>
